36fa8de6002394d3eac19f54cc4abb12a40e8c12
Gogotsmod <96406101+Gogotsmod@users.noreply.github.com>
2022-07-19 03:42:22 +0700

    Add files via upload

--- command/help.js
+++ command/help.js
@@ -15,22 +15,7 @@ function toCommas(x) {
 exports.allMenu = (ucapanWaktu, pushname, mundur, upload, download, ownerName, botName, jam, tanggal, runtime, isCreator, isPremium, sender, limitCount, limit, gcount, glimit, balance, prefix) => {
     return`${ucapanWaktu} ${pushname !== undefined ? pushname : 'Kak'}
 
-Hitung Mundur Idul Adha
-${mundur}
-
-*STATISTICS*
- • Upload : ${upload}
- • Downloads : ${download}
-
-*BOT INFO*
- • Creator : ${ownerName}
- • Bot Name : ${botName}
- • Time : ${jam}
- • Date : ${tanggal(new Date())}
- • Runtime : ${runtime(process.uptime())}
-
-🛡️ FF
-==============
+━━━━━━━━━━━━━━━━━━━━━━━
 🔰CHEAT FREE FIRE V1.91X🔰
 ━━━━━━━━━━━━━━━━━━━━━━━
 10 Hari 𝗣𝗦𝗛𝟰𝗫
@@ -38,6 +23,9 @@ Rp. 150.000 | $11
 
 20 Hari 𝗣𝗦𝗛𝟰𝗫
 Rp. 300.000 | $28
+
+Bulanan PFTEAM
+Rp. 150.000 | $11
 
 Bulanan 𝐅𝐅𝐇𝟒𝐗 
 Rp. 200.000 | $18
@@ -60,8 +48,7 @@ Rp. 200.000 | $20
 Bulanan Matrix Regedit PC/Emulator
 Rp. 350.000 | $30
 
-🛡️ML
-==============
+━━━━━━━━━━━━━━━━━━━━━━━
 🔰CHEAT MOBILAGEND 🔰
 ━━━━━━━━━━━━━━━━━━━━━━━
 Bulanan/Month ML+
@@ -73,34 +60,19 @@ Rp. 150.000 | $18
 Bulanan/Month NIU
 Rp. 150.000 | $18
 
-🛡️TESTIMONI
-==============
+━━━━━━━━━━━━━━━━━━━━━━━
+🔰TESTIMONI🔰
+━━━━━━━━━━━━━━━━━━━━━━━
 TESTIMONI: https://t.me/testimonipsteamadm
 
-🛡️TIKTOK
-==============
-Tiktok: https://vt.tiktok.com/ZSe1JhxJq/
 
-🛡️YOUTUBE
-==============
-Youtube: https://youtube.com/channel/UC5BkVIj4Z8ke2kg0AKmkGJg
-
-🛡️INSTAGRAM
-==============
-Instagram: https://www.instagram.com/tv/CaRCteQAuqb/?utm_medium=copy_link
-
-🛡️TELEGRAM
-==============
-Telegram: https://t.me/PSTeamADM
-
-Chat ke owner Ketik owner
 Admin akan membalas pesan anda secepatnya🙏
 `
 }
 
-exports.qris = (pushname, ownerNumber) => {
+exports.donate = (pushname, ownerNumber) => {
     return`Pembayaran 
-Dana ovo gopay
+Dana ovo gopay bank qris
 Nomer : 083846077142
 Contact  Owner:
 wa.me/${ownerNumber} (Owner)`
